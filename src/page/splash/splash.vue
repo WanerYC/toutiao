5149e4d5c517d25a869a49f6f8717b47725b4473
WanerYC <yangchun920726@126.com>
2019-05-24 16:34:57 +0800

    引入swiper使用
    在asstes中引入js不知为啥不能用 在static中引入可用 在滑动最后一个slide中跳入app页面中

--- src/page/splash/splash.vue
+++ src/page/splash/splash.vue
@@ -14,7 +14,7 @@
 
 <script>
 
-import'../../assets/swiper/swiper.min.js'
+import  Swiper from '../../../static/swiper/swiper.min.js'
 export default {
   data() {
     return {
@@ -25,51 +25,61 @@ export default {
     // setTimeout(() => {
     //   this.$router.push({name: 'home'})
     // },5000)
+    var _this = this
     var swiper = new Swiper('.swiper-container', {
       pagination: {
         el: '.swiper-pagination',
         dynamicBullets: true,
       },
+      on:{
+        touchEnd:function () {
+          // 判断是否在在最后一页继续拖动则释放跳转
+          if(swiper.isEnd) {
+            _this.$router.push({name: 'home'})
+
+          }
+        }
+      }
     });
   }
 }
 </script>
 
 <style>
-@import url("swiper.min.css");
+/* @import '../../assets/swiper/swiper.min.css'; */
 .splash {
-      position: relative;
-      height: 100vh;
-    }
-    .splash {
-      background: #eee;
-      font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
-      font-size: 14px;
-      color:#000;
-      margin: 0;
-      padding: 0;
-    }
-    .swiper-container {
-      width: 100%;
-      height: 100%;
-    }
-    .swiper-slide {
-      text-align: center;
-      font-size: 18px;
-      background: #fff;
+  position: relative;
+  height: 100vh;
+}
+.splash {
+  background: #eee;
+  font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  color:#000;
+  margin: 0;
+  padding: 0;
+}
+.swiper-container {
+  width: 100%;
+  height: 100%;
+}
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
 
-      /* Center slide text vertically */
-      display: -webkit-box;
-      display: -ms-flexbox;
-      display: -webkit-flex;
-      display: flex;
-      -webkit-box-pack: center;
-      -ms-flex-pack: center;
-      -webkit-justify-content: center;
-      justify-content: center;
-      -webkit-box-align: center;
-      -ms-flex-align: center;
-      -webkit-align-items: center;
-      align-items: center;
-    }
+  /* Center slide text vertically */
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  -webkit-align-items: center;
+  align-items: center;
+}
 </style>
